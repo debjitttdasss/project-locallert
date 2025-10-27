78e4a356061cb404f368258f5a15dbbe627492e9
DEBJIT DAS <226452850+debjitttdasss@users.noreply.github.com>
2025-10-27 23:20:08 +0530

    Create ArticleRepository.java

---
+++ Projects/capmass-backend/backend/src/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.campus.backend.repository;
+
+import com.campus.backend.model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
